8f8370f2bdf8bac6a8bdeff67f46c0910af73b29
medo962006 <ahmedadel01100847101@gmail.com>
2026-07-15 20:15:26 +0300

    fix: Increase QR/auth timeout to 120s/60s, add restartOnAuthFail

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -54,7 +54,10 @@ const client = new Client({
   webVersionCache: {
     type: 'remote',
     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
-  }
+  },
+  authTimeoutMs: 120000,
+  qrTimeoutMs: 60000,
+  restartOnAuthFail: true,
 });
 
 // ============================================================
